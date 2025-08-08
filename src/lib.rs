a7ae924c34445ac51ec7b65957969b5558663a6b
Graham Christensen <graham@grahamc.com>
2025-08-08 13:16:06 -0400

    Fixup 'ambiguous reference to positional arguments by number in a tuple variant; change this to a named argument' error

--- src/lib.rs
+++ src/lib.rs
@@ -174,9 +174,9 @@ impl NixConfig {
 pub enum ParseError {
     #[error("file '{0}' not found")]
     FileNotFound(PathBuf),
-    #[error("file '{0}' included from '{}' not found", .1.as_ref().map(|path| path.display().to_string()).unwrap_or(String::from("<unknown>")))]
+    #[error("file '{0}' included from '{origination}' not found", origination=.1.as_ref().map(|path| path.display().to_string()).unwrap_or(String::from("<unknown>")))]
     IncludedFileNotFound(PathBuf, Option<PathBuf>),
-    #[error("illegal configuration line '{0}' in '{}'", .1.as_ref().map(|path| path.display().to_string()).unwrap_or(String::from("<unknown>")))]
+    #[error("illegal configuration line '{0}' in '{origination}'", origination=.1.as_ref().map(|path| path.display().to_string()).unwrap_or(String::from("<unknown>")))]
     IllegalConfiguration(String, Option<PathBuf>),
     #[error("failed to read contents of '{0}': {1}")]
     FailedToReadFile(PathBuf, #[source] std::io::Error),
